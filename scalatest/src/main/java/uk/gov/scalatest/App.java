b96d9d342e34d052c9f49f8ef6dda9ad8a6252eb
Ajay <ajaykumar.k@gmail.com>
2017-04-04 20:11:04 +0100

    Project Structure

---
+++ scalatest/src/main/java/uk/gov/scalatest/App.java
@@ -0,0 +1,13 @@
+package uk.gov.scalatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
